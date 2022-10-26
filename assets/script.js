81f23c10d0b823e90e25fe7d299856bfdc12bde1
Edna Omadjambe <ednaomadjambe@Ednas-Air.attlocal.net>
2022-10-26 16:58:09 -0400

    updated js file

--- assets/script.js
+++ assets/script.js
@@ -7,13 +7,13 @@ const contact = document.querySelector('.contact');
 
 projects.addEventListener('click', function(){
 
-    window.location.href = 'https://edna1999.github.io/portfolio/pages/projects';
+    window.location.href = 'https://edna1999.github.io/portfolio/pages/projects/';
 
 });
 
 about.addEventListener('click', function(){
 
-   window.location.href = 'https://edna1999.github.io/portfolio/pages/about';
+   window.location.href = 'https://edna1999.github.io/portfolio/pages/about/';
 });
 
 home.addEventListener('click', function(){
